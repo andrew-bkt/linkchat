c4cc9560e07d11787199c8c81d78c44300c057d9
andrew-bkt <andrew@brooklyn-techology.com>
2024-09-15 15:18:00 -0400

    net

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
-// src/services/api.ts
-
+// frontend/src/services/api.ts
 import axios from 'axios';
 import supabase from '../utils/supabaseClient';
 
@@ -7,19 +6,36 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-// Add a request interceptor
 api.interceptors.request.use(
   async (config) => {
     const {
       data: { session },
     } = await supabase.auth.getSession();
+
     if (session && config.headers) {
-      console.log('Access Token:', session.access_token); // Debugging
+      console.log('Access Token:', session.access_token);
       config.headers['Authorization'] = `Bearer ${session.access_token}`;
     }
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('Error fetching chatbots:', error);
+    if (error.response) {
+      console.error('Data:', error.response.data);
+      console.error('Status:', error.response.status);
+      console.error('Headers:', error.response.headers);
+    } else if (error.request) {
+      console.error('Request:', error.request);
+    } else {
+      console.error('Error:', error.message);
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
