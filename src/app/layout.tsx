0a7a4084c6ce64f35a97e03af5cfe4e95c804939
andrew-bkt <andrew@brooklyn-techology.com>
2024-09-19 23:20:58 -0400

    speed insihts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { ReactNode } from 'react';
 import Providers from './Providers';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'LLM Chat Application',
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Providers>
           {children}
           <Analytics /> 
+          <SpeedInsights/>
         </Providers>
       </body>
     </html>
